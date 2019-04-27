febd0327eee95b73955275133851f964c3d77a0a
csabaiDomonkos <csabai.dermus@gmail.com>
2019-04-27 14:37:52 +0200

    Updates

--- precache-manifest.a8612dec437c918b87ee9bb830758cd9.js
+++ precache-manifest.a8612dec437c918b87ee9bb830758cd9.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/recipedb/static/js/runtime~main.7ba0f1b0.js"
   },
   {
-    "revision": "280c8a92a30b1eb9260a",
-    "url": "/recipedb/static/js/main.66ce0ebb.chunk.js"
+    "revision": "25743de528bd67741d3e",
+    "url": "/recipedb/static/js/main.5e5f0aea.chunk.js"
   },
   {
     "revision": "cc5c80ab7738d6eee2bb",
     "url": "/recipedb/static/js/2.de4dcf0f.chunk.js"
   },
   {
-    "revision": "280c8a92a30b1eb9260a",
+    "revision": "25743de528bd67741d3e",
     "url": "/recipedb/static/css/main.7efa507c.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/recipedb/static/css/2.4e9dd5c5.chunk.css"
   },
   {
-    "revision": "e7d894469657cc2107e46566019d927d",
+    "revision": "cf1e8baced85cf1b47a494806e41a295",
     "url": "/recipedb/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/recipedb/precache-manifest.3a3446f2df643130d84e31de4ef4adbf.js"
+  "/recipedb/precache-manifest.a8612dec437c918b87ee9bb830758cd9.js"
 );
 
 workbox.clientsClaim();
