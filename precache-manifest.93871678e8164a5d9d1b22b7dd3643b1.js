20283b475fa3cd093f36020ded2bf3e28de888b3
csabaiDomonkos <csabai.dermus@gmail.com>
2019-06-06 17:13:27 +0200

    Updates

--- precache-manifest.93871678e8164a5d9d1b22b7dd3643b1.js
+++ precache-manifest.93871678e8164a5d9d1b22b7dd3643b1.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/recipedb/static/js/runtime~main.7ba0f1b0.js"
   },
   {
-    "revision": "c13c7567db50c29b6088",
-    "url": "/recipedb/static/js/main.c7af7897.chunk.js"
+    "revision": "5a71a69642d76d79e044",
+    "url": "/recipedb/static/js/main.a3c57249.chunk.js"
   },
   {
-    "revision": "4b5a9d53e91abddb7240",
-    "url": "/recipedb/static/js/2.8a838476.chunk.js"
+    "revision": "c41b7dfe1d3239129914",
+    "url": "/recipedb/static/js/2.e5ab9d5c.chunk.js"
   },
   {
-    "revision": "c13c7567db50c29b6088",
+    "revision": "5a71a69642d76d79e044",
     "url": "/recipedb/static/css/main.08ae427b.chunk.css"
   },
   {
-    "revision": "4b5a9d53e91abddb7240",
+    "revision": "c41b7dfe1d3239129914",
     "url": "/recipedb/static/css/2.4e9dd5c5.chunk.css"
   },
   {
-    "revision": "58d875216449ee1a1fa92511cb791c89",
+    "revision": "f552abc8ea33aab191d66d1413a65283",
     "url": "/recipedb/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/recipedb/precache-manifest.c3c335276b1bdc6f7e9df18b1ec0d985.js"
+  "/recipedb/precache-manifest.93871678e8164a5d9d1b22b7dd3643b1.js"
 );
 
 workbox.clientsClaim();
