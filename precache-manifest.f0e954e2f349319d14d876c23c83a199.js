68629e4c2d2dc3a948d16928d4c1a6a7d4cd02d2
csabaiDomonkos <csabai.dermus@gmail.com>
2019-06-06 21:09:34 +0200

    Updates

---
+++ precache-manifest.f0e954e2f349319d14d876c23c83a199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d176db435951ebadc445232fb57404ed",
+    "url": "/recipedb/index.html"
+  },
+  {
+    "revision": "5506ee77cae90bbc60a0",
+    "url": "/recipedb/static/css/2.f9ce5b8e.chunk.css"
+  },
+  {
+    "revision": "8cfc56c41b787c654599",
+    "url": "/recipedb/static/css/main.8444ab70.chunk.css"
+  },
+  {
+    "revision": "5506ee77cae90bbc60a0",
+    "url": "/recipedb/static/js/2.a89a734e.chunk.js"
+  },
+  {
+    "revision": "8cfc56c41b787c654599",
+    "url": "/recipedb/static/js/main.2b1b3618.chunk.js"
+  },
+  {
+    "revision": "ebba5f25f3e77cc1752b",
+    "url": "/recipedb/static/js/runtime~main.7ba0f1b0.js"
+  }
+]);
